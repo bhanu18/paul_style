4be1f761bab7fdb557eb0a95b87856f4863113f5
Bhanuvidh Mansinghani <bhanuvidh@windowslive.com>
2024-03-27 04:22:17 +0000

    added nav menu list

---
+++ src/nav/nav.js
@@ -0,0 +1,14 @@
+function nav()
+{
+    return <>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Catalog</li>
+            <li>Order</li>
+            <li>About</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+    </>;
+}
