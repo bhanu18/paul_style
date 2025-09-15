29ad3fadf44a7019d82bb161930779ccd0d2adf1
bhanu18 <bhanuvidh@windowslive.com>
2025-09-15 22:45:42 +0700

    contact updates

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -12,8 +12,12 @@ function Body() {
           </div>
           <div className="col-md-6 col-sm-12">
             <div className="box center">
-              <p className="lead">Established in 1988, we offer traditional craftsmanship and outstanding workmanship, tailored to perfection.</p>
-              <p className="lead">Call us to schedule an appointment, we look forward to meet you
+              <p className="lead">
+                Established in 1988, we offer traditional craftsmanship and
+                outstanding workmanship, tailored to perfection.
+              </p>
+              <p className="lead">
+                Call us to schedule an appointment, we look forward to meet you
               </p>
             </div>
           </div>
@@ -29,7 +33,9 @@ function Body() {
             <div className="row">
               <div className="col text-center">
                 <h2>Finest Cloth</h2>
-                <p className="lead">First rate fabrics & highest quality materials</p>
+                <p className="lead">
+                  First rate fabrics & highest quality materials
+                </p>
               </div>
             </div>
             <div className="row">
@@ -41,10 +47,15 @@ function Body() {
             <div className="row">
               <div className="col text-center">
                 <h2>Appointment & Referrals</h2>
-                <p className="lead">Get appointment Contact through Email: <a href="mailto:mytailorpaul@hotmail.com">mytailorpaul@hotmail.com</a> or message on Whatsapp </p>
+                <p className="lead">
+                  Get appointment through Email:{" "}
+                  <a href="mailto:paulbespokesuits@gmail.com">
+                    paulbespokesuits@gmail.com
+                  </a>{" "}
+                  or Call us on 0936362549
+                </p>
               </div>
             </div>
-
           </div>
           <div className="col-sm-12 col-md-6 d-flex justify-content-center">
             <div className="img-container">
@@ -55,22 +66,24 @@ function Body() {
       </div>
       <div className="container mt-5 py-5 px-4 bg-white rounded">
         <div className="row">
-        <h5 className="mb-4 col-6">Interested in a suit? Contact Us</h5>
-        <h5 className="mb-4 col-6 text-right">Or visit us at</h5>
+          <h5 className="mb-4 col-6">Interested in a suit? Contact Us</h5>
+          <h5 className="mb-4 col-6 text-right">Or visit us at</h5>
         </div>
         <div className="row">
-        <Contact />
-        <div className="py-2 col-md-6 col-sm-1">
-                        <h5>A&A Collection Paseo LatKrabang</h5>
-                        <p>The Paseo Mall Latkrabang, 54-55 Inside Plaza, Opposite Watson, Lat Krabang, Bangkok 10520</p>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.871762843377!2d100.7269086!3d13.7203908!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d67160d4fa927%3A0x2db6e8cfc1bed99!2sA%26A%20Collection%20Paseo%20LatKrabang!5e0!3m2!1sen!2sth!4v1715089260307!5m2!1sen!2sth"
-                            width="100%"
-                            style={{ border: 0 }}
-                            allowfullscreen=""
-                            aria-hidden="false"
-                        />
-                    </div>
+          <Contact />
+          <div className="py-2 col-md-6 col-sm-1">
+            <h5>Paul Bespoke Suits</h5>
+            <p>
+              The Paseo Mall Latkrabang, 54 Inside Plaza, Opposite Watson, Lat
+              Krabang, Bangkok 10520
+            </p>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5834.141872009079!2d100.727029!3d13.7202782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d67df8d991bdb%3A0xbfbdaa1eb7b02d1a!2zUGF1bCBCZXNwb2tlIFN1aXRzIOC4iuC5iOC4suC4h-C4leC4seC4lOC4quC4ueC4lw!5e1!3m2!1sen!2sth!4v1757849877846!5m2!1sen!2sth"
+              width="100%"
+              style={{ border: 0 }}
+              aria-hidden="false"
+            />
+          </div>
         </div>
       </div>
     </>
